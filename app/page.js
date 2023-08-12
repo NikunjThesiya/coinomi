6e6fe14647e3c67807a0a60c300b3ba8f439e0d7
nikunjthesiya <nikunjthesiya.js@gmail.com>
2023-08-12 21:31:07 +0530

    colors added

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import HeroSection from "@/shared/sections/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			<HeroSection />
+		</main>
+	);
 }
